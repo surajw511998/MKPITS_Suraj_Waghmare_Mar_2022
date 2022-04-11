6241f55f71b880f1983c4a51fc488525c31f814b
surajw511998 <101802181+surajw511998@users.noreply.github.com>
2022-04-11 14:23:19 +0530

    Add files via upload

---
+++ Addition of two no.c
@@ -0,0 +1,13 @@
+/*addition of two numbers*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+
+{
+	float a,b,c;
+	printf("enter a,b");
+	scanf("%f%f",&a,&b);
+	c=a/b;
+	printf("the result is=%f",c);
+	getch();
+}
