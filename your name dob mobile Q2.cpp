05d25a418afdfc8cc4ea7729cd558048db80ff53
surajw511998 <101802181+surajw511998@users.noreply.github.com>
2022-04-11 14:22:40 +0530

    Add files via upload

---
+++ your name dob mobile Q2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+ int main()  
+  {
+     printf("Name   : suraj waghmare\n"); 
+     printf("DOB    : january 05 1998\n"); 
+     printf("Mobile : 9763961975\n"); 
+     return(0); 
+  }
+
