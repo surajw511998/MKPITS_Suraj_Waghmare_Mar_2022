b69cc1ae728e3d964ad993e27c7fea79fd9c5aa5
surajw511998 <101802181+surajw511998@users.noreply.github.com>
2022-04-11 14:30:36 +0530

    Add files via upload

---
+++ REVERSE.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	 int A,l=7,b=5;
+	 //printf("enterl,b"); 
+	//scanf("%d%d",&l,&b);
+	A=l*b;
+	printf("area of rectangle is =%d",A);
+	getch();
+	}
