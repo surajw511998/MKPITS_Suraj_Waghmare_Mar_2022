f744184b3107b7885645011ed3582b2a221d21a5
surajw511998 <101802181+surajw511998@users.noreply.github.com>
2022-05-01 10:08:24 +0530

    Add files via upload

---
+++ multiplication of two matrix.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	int m1[2][2], m2[2][2], res[2][2],i,j,k,sum=0;
+	printf("enter 2 X 2 matrix m1:\n");
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			scanf("%d",&m1[i][j]);
+		}
+	}
+		printf("enter 2 X 2 matrix m2:\n");
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			scanf("%d",&m2[i][j]);
+		}
+	}
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			for(k=0;k<=1;k++)
+			{
+				sum = sum+m1[i][k]*m2[k][j];
+			}
+			res[i][j]=sum;
+			sum=0;
+		}
+	}
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+		printf("m1[%d][%d]=%d\n",i,j,res[i][j]);
+		}
+	}
+	
+	
+}
