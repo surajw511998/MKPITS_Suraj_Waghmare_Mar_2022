fea32589caf23905d7e0e9ce4f999e71b3e125a4
surajw511998 <101802181+surajw511998@users.noreply.github.com>
2022-05-01 10:11:47 +0530

    Add files via upload

---
+++ copy the string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	char name1[15]={"abhijeet"};
+	char name2[15]={"krishna"};
+	strcpy(name1,name2);
+	printf("%s",name1);
+
+}
