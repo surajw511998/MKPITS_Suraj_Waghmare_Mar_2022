d9ca2d1d9f16884c21f7cb3b9b359a43dc7cdb7d
surajw511998 <101802181+surajw511998@users.noreply.github.com>
2022-05-01 10:24:58 +0530

    Add files via upload
    find simple interst

---
+++ find simple interest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int p,r,t;
+float si;
+printf("enter principle,rate and time of inetrest\n");
+scanf("%d%d%d",&p,&r,&t);
+si=(p*r*t)/100;
+printf("simple inetrest= %f",si);
+}
+
