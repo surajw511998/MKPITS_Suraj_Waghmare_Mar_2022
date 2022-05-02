bb39c08c6dd4f5b1350f7eb47ddd768cd4a0af7c
surajw511998 <101802181+surajw511998@users.noreply.github.com>
2022-05-02 10:40:25 +0530

    Add files via upload

---
+++ A1P4.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("  #####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n\n");
+	printf(" #####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf(" ####\n");
+	getch();
+	
+	
+}
