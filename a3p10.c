920656336c6013b8bea6b44b53667183163da9b4
surajw511998 <101802181+surajw511998@users.noreply.github.com>
2022-05-04 11:50:46 +0530

    Add files via upload

---
+++ a3p10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i=1,x;
+	printf("enter the integer number\n");
+	scanf("%d",&x);
+    printf("square of even values from 1 to %d\n",x);
+	while(i<=x)
+	{
+	if(i%2==0)
+	{
+	printf("%d^2 = %d\n",i,i*i);
+	}
+	i++;
+	}
+}
